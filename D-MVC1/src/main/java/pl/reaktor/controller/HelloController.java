3f7fade5b1255dbaafb210af26a1fed03ddba2c7
Hanka <h.kowerska@gmail.com>
2017-08-28 17:14:17 +0200

    pierwszy dzien springa

---
+++ D-MVC1/src/main/java/pl/reaktor/controller/HelloController.java
@@ -0,0 +1,15 @@
+package pl.reaktor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	// @GetMapping("adres")
+	@RequestMapping("/")
+	@ResponseBody
+	public String hello(){
+		return "hello";
+	}
+}
