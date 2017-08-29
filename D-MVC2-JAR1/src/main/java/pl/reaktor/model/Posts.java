e5a5a8c5cfbe6dd91bf0b2a0fb0d8a70ae130de2
Hanka <h.kowerska@gmail.com>
2017-08-29 12:44:38 +0200

    proba

---
+++ D-MVC2-JAR1/src/main/java/pl/reaktor/model/Posts.java
@@ -0,0 +1,34 @@
+package pl.reaktor.model;
+
+public class Posts {
+
+	private String title;
+	private String content;
+	private String sign;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getSign() {
+		return sign;
+	}
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+	public Posts(){}
+	public Posts(String title, String content, String sign) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.sign = sign;
+	}
+	
+}
---
+++ D-MVC3-Thymeleaf/src/main/java/pl/reaktor/DMvc3ThymeleafApplication.java
@@ -0,0 +1,12 @@
+package pl.reaktor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMvc3ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DMvc3ThymeleafApplication.class, args);
+	}
+}
